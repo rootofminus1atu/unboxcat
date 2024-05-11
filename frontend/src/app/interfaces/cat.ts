475fe1e980672e8e362921cd1a3f4fad0382e4b4
RootOfMinus1ATU <s00240122@atu.ie>
2024-05-12 00:23:03 +0100

    service

---
+++ frontend/src/app/interfaces/cat.ts
@@ -0,0 +1,23 @@
+interface Breed {
+    id: string;
+    name: string;
+    temperament: string[];
+    alt_names: string[];
+    origin: string;
+    country_code: string;
+    description: string;
+    wikipedia_url: string;
+    _id: string;
+}
+
+interface Cat {
+    _id: string;
+    imgUrl: string;
+    breed: Breed;
+    rarity: string;
+    petName: string;
+    fullName: string;
+    createdAt: string;
+    updatedAt: string;
+    __v: number;
+}
--- frontend/src/app/services/cat-api.service.ts
+++ frontend/src/app/services/cat-api.service.ts
@@ -1,9 +1,46 @@
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Result, err, ok } from 'neverthrow';
+import { Observable, catchError, map, of, tap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CatApiService {
+  private urlBase = "http://localhost:3000"
+  private url = (path: string) => `${this.urlBase}/cats${path}`
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getAllCats(): Observable<Result<Cat[], string>> {
+    return this.http.get<Cat[]>(this.url("/"))
+      .pipe(
+        tap(data => console.log('/cats/ message/error', JSON.stringify(data))),
+        map(cat => ok(cat)),
+        catchError(this.handleError)
+      )
+  }
+
+  getCatById(id: string): Observable<Result<Cat, string>> {
+    return this.http.get<Cat>(this.url(`/${id}`))
+      .pipe(
+        tap(data => console.log('/cats/:id message/error', JSON.stringify(data))),
+        map(cat => ok(cat)),
+        catchError(this.handleError)
+      )
+  }
+
+  getRandomCat(): Observable<Result<Cat, string>> {
+    return this.http.get<Cat>(this.url(`/random`))
+      .pipe(
+        tap(data => console.log('/cats/random message/error', JSON.stringify(data))),
+        map(cat => ok(cat)),
+        catchError(this.handleError)
+      )
+  }
+
+  private handleError (e: HttpErrorResponse): Observable<Result<never, string>> {
+    console.error('/cats error:', e.message);
+    return of(err(e.message))
+  }
 }
