fbfc31d35ab048600a80b64a5efa87f45e0dcd33
RootOfMinus1ATU <s00240122@atu.ie>
2024-05-13 19:31:22 +0100

    frontend more

--- frontend/src/app/components/cat-card/cat-card.component.ts
+++ frontend/src/app/components/cat-card/cat-card.component.ts
@@ -10,6 +10,19 @@ import { CommonModule } from '@angular/common';
   styleUrl: './cat-card.component.css'
 })
 export class CatCardComponent {
-  @Input() cat!: Cat;
+  @Input() cat!: Cat
+  showFullName = false
 
+  showModal(catId: any) {
+    const modalId = 'my_modal_' + catId;
+    // maybe if I coded my own modals I wouldn't have to do it but that's how it's done with this ui library
+    const modal = document.getElementById(modalId);
+    if (modal) {
+      (modal as any).showModal();
+    }
+  }
+
+  getCatClass() {
+    return 'cat-' + this.cat.rarity.toLowerCase()
+  }
 }
