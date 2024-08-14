c75394540887fb9d596444bc7127a5a7a988ff71
RootOfMinus1ATU <s00240122@atu.ie>
2024-08-14 23:38:55 +0100

    backend on deta

--- frontend/src/app/services/cat-api.service.ts
+++ frontend/src/app/services/cat-api.service.ts
@@ -8,7 +8,7 @@ import { Cat } from '../interfaces/cat';
   providedIn: 'root'
 })
 export class CatApiService {
-  private urlBase = "http://63.33.201.73:3000"
+  private urlBase = "https://unboxcatapi-1-b3149915.deta.app"
   // private urlBase = "http://localhost:3000"
   private url = (path: string) => `${this.urlBase}/cats${path}`
 
